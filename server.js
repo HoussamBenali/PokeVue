b178eeddce8e4a353eb1cd3079f8a10366fb42f7
unknown <houssam.benali.28@lacetania.cat>
2021-04-26 14:29:21 +0200

    PokeVue

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 
 const app = express();
 
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(serveStatic(path.join(__dirname, '/dist')));
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 app.listen(port);
